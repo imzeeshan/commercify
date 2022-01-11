c03acd01e0bda5a2eef8058f78c1c1b95e6de8fd
Zeeshan Chawdhary <imzeeshanc@gmail.com>
2022-01-11 14:52:22 +0530

    Create 2022_01_11_082641_create_products_table.php
    Products table

---
+++ Modules/Products/Database/Migrations/2022_01_11_082641_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('sku');
+            $table->boolean('type')->default(0)->comment('0: simple, 1: variable');
+            $table->jsonb('category');
+            $table->jsonb('tags');
+            $table->jsonb('images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
